9ebc907967b78073365eb3523cb41ac5b35370dd
WYJ5032 <dudwns5032@gmail.com>
2024-07-26 17:37:31 +0900

    music 반응형 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Main from '../components/section/Main'
-import Developer from '../components/contents/Developer'
 import Gsap from '../components/contents/Gsap'
 import Portfolio from '../components/contents/Portfolio'
 import Today from '../components/contents/Today'
 import Webd from '../components/contents/Webd'
 import Website from '../components/contents/Website'
 import Youtube from '../components/contents/Youtube'
+import Music from '../components/contents/Music'
 
 const Home = () => {
   return (
     <Main title="유튜브 채널" description="유튜브 채널 페이지입니다.">
       <Today/>
-      <Developer/>
+      <Music/>
       <Webd/>
       <Website/>
       <Gsap/>
